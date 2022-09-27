f44d046db81b914f17bf3d416841f0cd65551bda
Arya Suwandika <aryaswan17@gmail.com>
2022-09-27 15:20:00 +0800

    added better alert function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const tanggal = new Date;
+const liveAlert = document.getElementById("diaryAlert");
 const diaryKey = "diaryEntry";
 let diaryEntry = "<em>Belum ada apa-apa disini...</em>";
 const diaryDateKey = "diaryDate";
@@ -9,6 +10,23 @@ if (localStorage.getItem(diaryKey) === null) {
 if (localStorage.getItem(diaryDateKey) === null) {
     localStorage.setItem(diaryDateKey, diaryDate)
 }
+
+const diaryAlert = (message) => {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = [
+    `<div class="alert alert-primary alert-dismissible mx-2" role="alert">`,
+    `   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">`,
+    `       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>`,
+    `       <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>`,
+    `   </svg>`,
+    `   ${message}`,
+    `   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`,
+    `</div>`
+    ].join('')
+
+    liveAlert.append(wrapper);
+}
+
 
 document.addEventListener("load", dateLoad());
 document.addEventListener("load", retrieveEntry());
@@ -40,14 +58,15 @@ function deleteEntry(option) {
             document.getElementById("unek-unek").innerHTML = "<em>Belum ada apa-apa disini...</em>"
             localStorage.setItem(diaryKey, "<em>Belum ada apa-apa disini...</em>");
             document.getElementById("hapus").style.display = "none";
-            alert("Diary sudah dihapus!");
+            diaryAlert("Diary sudah dihapus!");
         }
     }
     if (option == "auto") {
         document.getElementById("unek-unek").innerHTML = "<em>Belum ada apa-apa disini...</em>"
         localStorage.setItem(diaryKey, "<em>Belum ada apa-apa disini...</em>");
         document.getElementById("hapus").style.display = "none";
-        alert("Diary sudah reset!");
+        diaryAlert("Diary sudah direset!");
+        setTimeout(10000, )
     }
 }
 
@@ -63,6 +82,7 @@ function retrieveEntry() {
 
 function loadEntry() {
     document.getElementById("unek-unek").innerHTML = diaryEntry;
+    document.getElementById("input").value = "";
 }
 
 function dateLoad() {
